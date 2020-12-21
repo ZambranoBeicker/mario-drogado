fdc27670d40a3e92c45c07280d82a3f14d15999b
Beicker Zambrano <beicker998@gmail.com>
2020-12-21 17:00:14 -0500

    Start to add physics to the game

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,21 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
+
+
 {
+
+    public Rigidbody2D body;
     // Start is called before the first frame update
     void Start()
     {
+			body = GetComponent<Rigidbody2D>();
 
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log("It's working!!");
-        if (Input.GetKey(KeyCode.UpArrow))
+
+        if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("your key is UpArrow");
+            Debug.Log("The key 'W' was successfully pressed");
+            body.velocity = Vector2.up * 10;
         }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("The key 'W' was successfully pressed");
+            body.velocity = Vector2.right * 10;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("The key 'W' was successfully pressed");
+            body.velocity = Vector2.left * 10;
+        }
+        //if (Input.GetKey(KeyCode.S))
+        //{
+            //Debug.Log("The key 'W' was successfully pressed");
+            //body.velocity = Vector2.down * 10;
+        //}
     }
 }
