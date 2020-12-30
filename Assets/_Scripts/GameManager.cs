1734fc2f37ea19c35059a3fce989c76df0db4a70
mijaelrev <mijael_qwert@hotmail.com>
2020-12-30 00:05:12 -0500

    Add Simple Update

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EGameState
+{
+    menu, inGame, gameOver
+}
+
+public class GameManager : MonoBehaviour
+{
+    public EGameState CurrentGameState = EGameState.menu;
+    public static GameManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        
+    }
+    private void Update()
+    {
+        
+    }
+    public void Menu()
+    {
+        SetGameState(EGameState.menu);
+    }
+    public void StarGame()
+    {
+        SetGameState(EGameState.inGame);
+    }
+
+    public void GameOver()
+    {
+        SetGameState(EGameState.gameOver);
+    }
+    private void SetGameState(EGameState newGameState)
+    {
+        if (newGameState == EGameState.menu)
+        {
+
+        }
+        else if (newGameState == EGameState.inGame)
+        {
+
+        }
+        else if (newGameState == EGameState.gameOver)
+        {
+
+        }
+
+        this.CurrentGameState = newGameState;
+    }
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -2,68 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
     private BoxCollider2D box2D;
-    private float speed = 500f, runSpeed = 1000f, jumpSpeed = 7.5f, climp = 10f;
-    public float distanceToGround = 0.1f;
+    private SpriteRenderer mario;
+    [SerializeField] private float speed = 480f,runSpeed = 1000f, jumpSpeed = 280f, climp = 2.5f;
+    //private float distanceToGround = 0.1f;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         box2D = GetComponent<BoxCollider2D>();
+        mario = GetComponent<SpriteRenderer>();
     }
-    void Start()
+    private void Start()
     {
+        anim = GetComponent<Animator>();
         Cursor.lockState = CursorLockMode.Locked;
-
     }
-    void Update()
+    private void Update()
     {
         if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
+            anim.SetFloat("Walking", 1f);
             rb.AddForce(new Vector2(-speed * Time.deltaTime, 0));
+            mario.flipX = true;
         }
         if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
         {
+            anim.SetFloat("Walking", -1f);
             rb.AddForce(new Vector2(speed * Time.deltaTime, 0));
+            mario.flipX = false;
         }
         if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
         {
-            rb.AddForce(Vector2.up * climp * Time.deltaTime, ForceMode2D.Force);
+            rb.velocity = new Vector2(0, climp);
         }
         if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
         {
-            rb.AddForce(new Vector2(0, climp * Time.deltaTime));
+            rb.velocity = new Vector2(0, -climp);
         }
         if (Input.GetButton("Sprint") && Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) || Input.GetButton("Sprint") && Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.S))
         {
             Runner();
         }
 
-        //El salto al multiplicarlo con el Time.deltaTime se va a la mrd, el Time.deltaTime es un comando importante para que no varien las fisicas en un algun dispositovo con potencia extremadamente baja o una con mucha
-        //Aun asi al aplicarlo al salto este hace saltos extraños, la unica solucion que veo es limitar los FPS
+        //para evitar errores mas grandes es necesario movelo a FixedUpdate o establecer un limite en los cuadros por segundo
         /*if (BoolJump() && Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(0f, jumpSpeed);
+            rb.AddForce = new Vector2(0f, jumpSpeed);
         }*/
     }
     private void FixedUpdate()
     {
-        //TODO reparar el salto
+        //TODO: reparar el salto e Input
         if (BoolJump() && Input.GetButton("Jump"))
         {
-            rb.velocity = new Vector2(0f, jumpSpeed);
+            rb.AddForce(new Vector2(0, jumpSpeed));
         }
     }
-    bool BoolJump()
+    private bool BoolJump()
     {
+        //Posibles modificaciones a un Void y encapsulamiento
         Vector2 capsuleBottom = new Vector2(box2D.bounds.center.x, box2D.bounds.min.y);
         bool grounded = Physics2D.IsTouchingLayers(box2D);
         return grounded;
     }
-    void Runner()
+    private void Runner()
     {
+        //TODO: Boton para Correr en alfa, es necesario que vuelva a su valor inicial una vez el boton deje de ser precionado
         Vector2 runnerVector = new Vector2(runSpeed, jumpSpeed + 2);
     }
 }
