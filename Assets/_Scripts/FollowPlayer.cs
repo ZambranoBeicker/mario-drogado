620ca66be626161094e146603aa45ee80e954634
mijaelrev <mijael_qwert@hotmail.com>
2021-01-01 13:20:56 -0500

    Add Camera Script Beta

---
+++ Assets/_Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject thePlayer;
+    private Vector3 offSet = new Vector3(0, 1, -15f);
+    private void Awake()
+    {
+    }
+    void Start()
+    {
+    }
+    private void Update()
+    {
+        this.transform.position = thePlayer.transform.position + offSet;
+    }
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -10,57 +10,57 @@ public class PlayerController : MonoBehaviour
     private Animator anim;
     private BoxCollider2D box2D;
     private SpriteRenderer mario;
-    [SerializeField] private float speed = 480f,runSpeed = 1000f, jumpSpeed = 550f, climp = 2.5f;
+    [SerializeField, Range(1f, 1000f), Tooltip("Velocidades Del Player")]
+    private float speed = 480f,runSpeed = 1000f, jumpSpeed = 380f, climp = 2.5f;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         box2D = GetComponent<BoxCollider2D>();
         mario = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
     private void Start()
     {
-        anim = GetComponent<Animator>();
         Cursor.lockState = CursorLockMode.Locked;
     }
     private void Update()
     {
         if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
-            rb.AddForce(new Vector2(-speed * Time.deltaTime, 0));
+            rb.AddForce(new Vector2(-speed * Time.deltaTime, 0f));
             mario.flipX = true;
         }
         if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(new Vector2(speed * Time.deltaTime, 0));
+            rb.AddForce(new Vector2(speed * Time.deltaTime, 0f));
             mario.flipX = false;
         }
-        //TODO: Añadir una Funcion en OntriggerEnter2D en Raiz ascendente(puede violar la gravedad) y no violar la gravedad
+        //TODO: Añadir una Funcion en OntriggerEnter2D en Raiz ascendente(puede violar la gravedad al precionar el botton W y desactivarlo al saltar)
         if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
         {
-            rb.velocity = new Vector2(0, climp);
+            rb.velocity = new Vector2(0f, climp);
         }
-        //TODO: Añadir una Funcion en OntriggerEnter2D Raiz ascendente(Puede violar la gravedad) y para entrar a un tuvo(ignora las colisiones y la gravedad)
+        //TODO: Añadir una Funcion en OntriggerEnter2D en Raiz ascendente(puede violar la gravedad al precionar el botton W y desactivarlo al saltar)
+        //y para entrar a un tuvo(ignora las colisiones y la gravedad)
         if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.D))
         {
-            rb.velocity = new Vector2(0, -climp);
+            rb.velocity = new Vector2(0f, -climp);
         }
         if (Input.GetButton("Sprint") && Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) || Input.GetButton("Sprint") && Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.S))
         {
             Runner();
         }
-        //TODO:Reparar
         if (BoolJump() && Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(new Vector2(0, jumpSpeed));
+            rb.AddForce(new Vector2(0f, jumpSpeed));
         }
     }
     private void FixedUpdate()
     {
-        //TODO: reparar el salto e Input
-        /*if (BoolJump() && Input.GetButton("Jump"))
+        if (Input.GetButton("Jump"))
         {
-            rb.AddForce(new Vector2(0, jumpSpeed));
-        }*/
+            rb.AddForce(new Vector2(0f, 10f));
+        }
     }
     private bool BoolJump()
     {
